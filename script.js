1a0d9834d11f1a12cbf4b1517eedc365f2cc57a6
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-07-05 15:29:44 +0200

    -Create displayBook function template. -Add book display template for html.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let awsomeBooks = [];
+let awesomeBooks = [];
 
 function Book(bookTitle, bookAuthor) {
   (this.bookTitle = bookTitle), (this.bookAuthor = bookAuthor);
 }
 
 function addBook(bookTitle, bookAuthor) {}
+function displayBook(book){
+  
+}
+
+//div>div>div*2>h3+h2^+btn

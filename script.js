a18e2193f12508695975474677a2eafe5f1d142b
PolinaStamenova <polina.stamenova@gmail.com>
2021-07-07 13:19:25 +0300

    Change card to list in javascript.js, add styling into index.css

--- script.js
+++ script.js
@@ -1,51 +1,30 @@
 /* eslint-disable max-classes-per-file, no-use-before-define */
 
 let bookIncrement = 0;
-if (localStorage.getItem('bookIncrement')) {
-  bookIncrement = localStorage.getItem('bookIncrement');
+if (localStorage.getItem("bookIncrement")) {
+  bookIncrement = localStorage.getItem("bookIncrement");
 }
 
 function displayBook(...args) {
-  const tempArray = ['Title', 'Author'];
-  const mainContainer = document.getElementById('BookContainer');
-  const container = document.createElement('div');
+  const [bookTitle, bookAuthor, selectedBook] = args;
+
+  const mainContainer = document.getElementById("BookContainer");
+  const container = document.createElement("li");
   container.id = `book${args[2]}`;
-  container.classList.add(
-    'container',
-    'mx-auto',
-    'border',
-    'rounded',
-    'justify-content-center',
-    'my-2',
-  );
-  for (let i = 0; i < tempArray.length; i += 1) {
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add(
-      'col-md-8',
-      'text-center',
-      'justify-content-center',
-      'mx-auto',
-      'my-2',
-      'd-flex',
-    );
-    const titleLabel = document.createElement('h3');
-    titleLabel.classList.add('h3', 'mx-2');
-    titleLabel.textContent = `${tempArray[i]}:`;
-    titleContainer.appendChild(titleLabel);
-    const bookTitle = document.createElement('h2');
-    bookTitle.classList.add('h2', 'mx-2');
-    bookTitle.innerText = args[i];
-    titleContainer.appendChild(bookTitle);
-    container.appendChild(titleContainer);
-  }
-  const removeButton = document.createElement('button');
-  const [, , selectedBook] = args;
+  container.classList.add("container", "container-style");
+
+  const contentTitle = document.createElement("p");
+  contentTitle.classList.add("title-style");
+  contentTitle.textContent = `'${bookTitle}' by ${bookAuthor}`;
+  container.appendChild(contentTitle);
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("btn", "col-5", "my-2", "button-style");
   removeButton.id = selectedBook;
-  removeButton.classList.add('btn', 'btn-primary', 'col-5', 'my-2');
-  removeButton.innerText = 'Remove Book';
+  removeButton.innerText = "Remove";
   container.appendChild(removeButton);
   mainContainer.appendChild(container);
-  removeButton.addEventListener('click', () => {
+  removeButton.addEventListener("click", () => {
     list.deleteBook(selectedBook);
   });
 }
@@ -61,8 +40,8 @@ class Booklist {
     displayBook(book.bookTitle, book.bookAuthor, book.bookId);
     bookIncrement += 1;
     localStorage.clear();
-    localStorage.setItem('awesomeBooks', JSON.stringify(this.awesomeBooks));
-    localStorage.setItem('bookIncrement', bookIncrement);
+    localStorage.setItem("awesomeBooks", JSON.stringify(this.awesomeBooks));
+    localStorage.setItem("bookIncrement", bookIncrement);
   }
 
   deleteBook(id) {
@@ -70,7 +49,7 @@ class Booklist {
     container.remove();
     this.awesomeBooks = this.awesomeBooks.filter((book) => book.bookId !== id);
     localStorage.clear();
-    localStorage.setItem('awesomeBooks', JSON.stringify(this.awesomeBooks));
+    localStorage.setItem("awesomeBooks", JSON.stringify(this.awesomeBooks));
   }
 }
 
@@ -83,15 +62,15 @@ class Book {
 }
 
 function getBooks() {
-  const tempStorage = JSON.parse(localStorage.getItem('awesomeBooks'));
+  const tempStorage = JSON.parse(localStorage.getItem("awesomeBooks"));
   if (tempStorage) {
     list.awesomeBooks = tempStorage;
   }
 }
 
 let list = new Booklist();
-if (localStorage.getItem('awesomeBooks')) {
-  list = new Booklist(JSON.parse(localStorage.getItem('awesomeBooks')));
+if (localStorage.getItem("awesomeBooks")) {
+  list = new Booklist(JSON.parse(localStorage.getItem("awesomeBooks")));
 }
 
 getBooks();
@@ -108,17 +87,19 @@ function getBookId() {
 }
 getBookId();
 
-document.getElementById('SubmitButton').addEventListener('click', () => {
-  const bookTitleInput = document.getElementById('BookTitle').value;
-  const bookAuthorInput = document.getElementById('BookAuthor').value;
+document.getElementById("SubmitButton").addEventListener("click", () => {
+  const bookTitleInput = document.getElementById("BookTitle").value;
+  const bookAuthorInput = document.getElementById("BookAuthor").value;
 
   list.addBook(new Book(bookTitleInput, bookAuthorInput, bookIncrement));
 });
 
 function printStorage() {
-  if (localStorage.getItem('awesomeBooks')) {
-    list.awesomeBooks.forEach((book) => displayBook(book.bookTitle, book.bookAuthor, book.bookId));
+  if (localStorage.getItem("awesomeBooks")) {
+    list.awesomeBooks.forEach((book) =>
+      displayBook(book.bookTitle, book.bookAuthor, book.bookId)
+    );
   }
 }
 
-document.addEventListener('DOMContentLoaded', printStorage());
+document.addEventListener("DOMContentLoaded", printStorage());

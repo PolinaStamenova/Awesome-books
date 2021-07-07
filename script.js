11c15a7b45985df7e6c3552196621269763463bb
PolinaStamenova <polina.stamenova@gmail.com>
2021-07-07 15:44:49 +0300

    Fix Linter errors

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 /* eslint-disable max-classes-per-file, no-use-before-define */
 
 let bookIncrement = 0;
-if (localStorage.getItem("bookIncrement")) {
-  bookIncrement = localStorage.getItem("bookIncrement");
+if (localStorage.getItem('bookIncrement')) {
+  bookIncrement = localStorage.getItem('bookIncrement');
 }
 
 function displayBook(...args) {
   const [bookTitle, bookAuthor, selectedBook] = args;
-  const mainContainer = document.getElementById("BookContainer");
-  const container = document.createElement("li");
+  const mainContainer = document.getElementById('BookContainer');
+  const container = document.createElement('li');
   container.id = `book${selectedBook}`;
-  container.classList.add("container", "container-style");
-  const contentTitle = document.createElement("p");
-  contentTitle.classList.add("title-style");
+  container.classList.add('container', 'container-style');
+  const contentTitle = document.createElement('p');
+  contentTitle.classList.add('title-style');
   contentTitle.textContent = `"${bookTitle}" by ${bookAuthor}`;
   container.appendChild(contentTitle);
-  const removeButton = document.createElement("button");
-  removeButton.classList.add("btn", "col-5", "my-2", "button-style");
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('btn', 'col-5', 'my-2', 'button-style');
   removeButton.id = selectedBook;
-  removeButton.innerText = "Remove";
+  removeButton.innerText = 'Remove';
   container.appendChild(removeButton);
   mainContainer.appendChild(container);
-  removeButton.addEventListener("click", () => {
+  removeButton.addEventListener('click', () => {
     list.deleteBook(selectedBook);
   });
 }
@@ -36,8 +36,8 @@ class Booklist {
     displayBook(book.bookTitle, book.bookAuthor, book.bookId);
     bookIncrement += 1;
     localStorage.clear();
-    localStorage.setItem("awesomeBooks", JSON.stringify(this.awesomeBooks));
-    localStorage.setItem("bookIncrement", bookIncrement);
+    localStorage.setItem('awesomeBooks', JSON.stringify(this.awesomeBooks));
+    localStorage.setItem('bookIncrement', bookIncrement);
   }
 
   deleteBook(id) {
@@ -45,7 +45,7 @@ class Booklist {
     container.remove();
     this.awesomeBooks = this.awesomeBooks.filter((book) => book.bookId !== id);
     localStorage.clear();
-    localStorage.setItem("awesomeBooks", JSON.stringify(this.awesomeBooks));
+    localStorage.setItem('awesomeBooks', JSON.stringify(this.awesomeBooks));
   }
 }
 
@@ -58,15 +58,15 @@ class Book {
 }
 
 function getBooks() {
-  const tempStorage = JSON.parse(localStorage.getItem("awesomeBooks"));
+  const tempStorage = JSON.parse(localStorage.getItem('awesomeBooks'));
   if (tempStorage) {
     list.awesomeBooks = tempStorage;
   }
 }
 
 let list = new Booklist();
-if (localStorage.getItem("awesomeBooks")) {
-  list = new Booklist(JSON.parse(localStorage.getItem("awesomeBooks")));
+if (localStorage.getItem('awesomeBooks')) {
+  list = new Booklist(JSON.parse(localStorage.getItem('awesomeBooks')));
 }
 
 getBooks();
@@ -83,19 +83,17 @@ function getBookId() {
 }
 getBookId();
 
-document.getElementById("SubmitButton").addEventListener("click", () => {
-  const bookTitleInput = document.getElementById("BookTitle").value;
-  const bookAuthorInput = document.getElementById("BookAuthor").value;
+document.getElementById('SubmitButton').addEventListener('click', () => {
+  const bookTitleInput = document.getElementById('BookTitle').value;
+  const bookAuthorInput = document.getElementById('BookAuthor').value;
 
   list.addBook(new Book(bookTitleInput, bookAuthorInput, bookIncrement));
 });
 
 function printStorage() {
-  if (localStorage.getItem("awesomeBooks")) {
-    list.awesomeBooks.forEach((book) =>
-      displayBook(book.bookTitle, book.bookAuthor, book.bookId)
-    );
+  if (localStorage.getItem('awesomeBooks')) {
+    list.awesomeBooks.forEach((book) => displayBook(book.bookTitle, book.bookAuthor, book.bookId));
   }
 }
 
-document.addEventListener("DOMContentLoaded", printStorage());
+document.addEventListener('DOMContentLoaded', printStorage());
